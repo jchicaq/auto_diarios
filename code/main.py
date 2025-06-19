c7f7065b05cfbb81eefaa9871fe7be56f3110289
Jacobo Chica Quintero <jacobochica13@gmail.com>
2025-06-19 17:16:55 -0500

    Ajustes del parner
    Ya terminamos el dataframe de link, comenzamos a hacer el dataframe de partner, es hacerle seguimiento y ver como nos termina de ir

--- code/main.py
+++ code/main.py
@@ -18,42 +18,71 @@ import funciones_sql as fsql
 # Ruta del archivo CSV de links
 archivo_link = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data/links_resumen_diario.csv")
 
+# Ruta del archivo CSV de partner
+archivo_partner = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data/reporte_partner_afiliados.csv")
+
 # Ruta del archivo de la base de datos SQLite
 ruta_db = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data_base/datos_diario_digital.db")
 
+
+
 # Nombres de las tablas en la base de datos SQLite
 tabla_links = "links_diario"
+tabla_partner = "partner_diario"
+
 
 
 # Se obtiene el DataFrame inicial a partir del archivo CSV de links
 df_inicial_link = flink.insertar_archivo_csv(archivo_link)
 
-
-# Variables para almacenar los nombres de las columnas
-col_periodo             = df_inicial_link.columns[0]
-col_partner             = df_inicial_link.columns[1]
-col_pais                = df_inicial_link.columns[2]
-col_id_link             = df_inicial_link.columns[3]
-col_nombre_link         = df_inicial_link.columns[4]
-col_id_afiliador        = df_inicial_link.columns[5]
-col_nombre_afiliador    = df_inicial_link.columns[6]
-col_registros           = df_inicial_link.columns[7]
-col_ftd                 = df_inicial_link.columns[8]
-col_valor_ftd           = df_inicial_link.columns[9]
-col_ftd_puntos_venta    = df_inicial_link.columns[10]
-col_ftd_pasarela        = df_inicial_link.columns[11]
+# Se obtiene el DataFrame inicial a partir del archivo CSV de partner
+df_inicial_partner = flink.insertar_archivo_csv(archivo_partner)
 
 
-# Se organizan las columnas del DataFrame inicial
-df_inicial_link = flink.convertir_fechas(df_inicial_link, col_periodo)
+
+# Variables para almacenar los nombres de las columnas link (c: columnas, lin: link)
+clin_periodo             = df_inicial_link.columns[0]
+clin_partner             = df_inicial_link.columns[1]
+clin_pais                = df_inicial_link.columns[2]
+clin_id_link             = df_inicial_link.columns[3]
+clin_nombre_link         = df_inicial_link.columns[4]
+clin_id_afiliador        = df_inicial_link.columns[5]
+clin_nombre_afiliador    = df_inicial_link.columns[6]
+clin_registros           = df_inicial_link.columns[7]
+clin_ftd                 = df_inicial_link.columns[8]
+clin_valor_ftd           = df_inicial_link.columns[9]
+clin_ftd_puntos_venta    = df_inicial_link.columns[10]
+clin_ftd_pasarela        = df_inicial_link.columns[11]
+
+# Variables para almacenar los nombres de las columnas partner (co: columnas, par: partner)
+cpar_periodo            = df_inicial_partner.columns[0]
+cpar_partner            = df_inicial_partner.columns[1]
+cpar_pais               = df_inicial_partner.columns[2]
+cpar_usuarios_nuevos    = df_inicial_partner.columns[3]
+cpar_ftd                = df_inicial_partner.columns[4]
+cpar_usuarios_deposito  = df_inicial_partner.columns[5]
+cpar_activo_deportivas  = df_inicial_partner.columns[6]
+cpar_activo_casino      = df_inicial_partner.columns[7]
+cpar_ftd_pasarela       = df_inicial_partner.columns[8]
+
+
+
+# Se organizan las columnas del DataFrame inicial de link
+df_inicial_link = flink.convertir_fechas(df_inicial_link, clin_periodo)
+
+# se organizan las columnas del DataFrame inicial de partner
+def_inicial_partner = flink.convertir_fechas(df_inicial_partner, cpar_periodo)
+
 
 
 # Se convierten las variables de la columna partnern y pais
-df_link = flink.convertir_variables(df_inicial_link, col_partner, col_pais)
+df_link = flink.convertir_variables(df_inicial_link, clin_partner, clin_pais)
+
+# 
 
 
 # Se convierten las columnas de tipo numérico a float
-df_link = flink.columnas_numericas(df_link, col_registros, col_ftd, col_valor_ftd, col_ftd_puntos_venta, col_ftd_pasarela)
+df_link = flink.columnas_numericas(df_link, clin_registros, clin_ftd, clin_valor_ftd, clin_ftd_puntos_venta, clin_ftd_pasarela)
 
 
 # Guardar el archivo CSV de links procesado en un archivo SQLite
