a8f33293a867c839f505a0e86467cde2edd0b85a
Jacobo Chica Quintero <jacobochica13@gmail.com>
2025-06-19 15:44:25 -0500

    Limpieza de datos y creación de la base de datos
    Se implemento el método de trabajo con un archivo main.py principal y las funciones según sean el proceso se trabajan en los diferentes archivos de funciones.
    
     Se implemento el trabajo por Variables para que el código sea mucho más consistente.

--- code/main.py
+++ code/main.py
@@ -1,92 +1,60 @@
 # ----------------------------------------------------------------------
 # Autor: Jacobo Chica
 #
-# Fecha: 18/06/2025
+# Fecha: 19/06/2025
 #
-# Descripción: Script inicial para la automatización de los informes diarios
-# en donde se leen los archivos CSV y se preparan para su procesamiento.
+# Descripción: Este archivo es el punto de entrada principal para la automatización de los informes diarios.
+# Se encarga de leer los archivos de links y resumen necesarios y preparar los datos para su posterior procesamiento.
+# Desde este script se ejecutan las funciones definidas en otros módulos del proyecto.
 # ----------------------------------------------------------------------
 
 # Importar las librerías necesarias
 import pandas as pd
-import re
 from pathlib import Path
+import funciones_link as flink
+import funciones_sql as fsql
 
 
-# Definir la ruta del archivo CSV y verificar su existencia
-try:
-    archivo = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data/links_resumen_diario.csv")
-    if not archivo.exists():
-        print("No se encontró el archivo.")
-        exit(1)
-except Exception as e:
-    print("Ocurrió un error al intentar acceder al archivo:", e)
-    exit(1)
+# Ruta del archivo CSV de links
+archivo_link = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data/links_resumen_diario.csv")
+
+# Ruta del archivo de la base de datos SQLite
+ruta_db = Path("C:/Users/PRUEBA/Documents/GitHub/Digital/aut_diarios/data_base/datos_diario_digital.db")
+
+# Nombres de las tablas en la base de datos SQLite
+tabla_links = "links_diario"
 
 
-# Leer el CSV con separador ';', permitir comillas dobles
-df_inicial = pd.read_csv(
-    archivo,
-    sep=';',
-    quotechar='"',
-    dtype=str,  
-    encoding='utf-8',
-    skip_blank_lines=True,
-    engine='python'
-    )
+# Se obtiene el DataFrame inicial a partir del archivo CSV de links
+df_inicial_link = flink.insertar_archivo_csv(archivo_link)
 
 
-# Copia del dataframe para probar formato con dayfirst=True y encontrar la fecha de corte
-df_inicial_copia = df_inicial.copy()
-df_inicial_copia['Periodo'] = pd.to_datetime(df_inicial_copia['Periodo'], dayfirst=True, errors='coerce')
+# Variables para almacenar los nombres de las columnas
+col_periodo             = df_inicial_link.columns[0]
+col_partner             = df_inicial_link.columns[1]
+col_pais                = df_inicial_link.columns[2]
+col_id_link             = df_inicial_link.columns[3]
+col_nombre_link         = df_inicial_link.columns[4]
+col_id_afiliador        = df_inicial_link.columns[5]
+col_nombre_afiliador    = df_inicial_link.columns[6]
+col_registros           = df_inicial_link.columns[7]
+col_ftd                 = df_inicial_link.columns[8]
+col_valor_ftd           = df_inicial_link.columns[9]
+col_ftd_puntos_venta    = df_inicial_link.columns[10]
+col_ftd_pasarela        = df_inicial_link.columns[11]
 
 
-# Encontrar la primera fila con fecha no reconocida (NaT)
-fecha_corte = df_inicial_copia['Periodo'].isna().idxmax()
-print(f"\n 🔍 Cambio de formato detectado en el índice: {fecha_corte}\n")
+# Se organizan las columnas del DataFrame inicial
+df_inicial_link = flink.convertir_fechas(df_inicial_link, col_periodo)
 
 
-# Separar el DataFrame en dos partes para manejar diferentes formatos de fecha 
-df_1 = df_inicial.iloc[:fecha_corte].copy()   # Parte con fechas en formato dd/mm/yyyy
-df_2 = df_inicial.iloc[fecha_corte:].copy()   # Parte con fechas en formato yyyy-mm-dd
+# Se convierten las variables de la columna partnern y pais
+df_link = flink.convertir_variables(df_inicial_link, col_partner, col_pais)
 
 
-print("Número de filas en df_1:", len(df_1))
-print("Número de filas en df_2:", len(df_2))
-
-df_1['Periodo'] = pd.to_datetime(df_1['Periodo'], dayfirst=True, errors='coerce')
-df_2['Periodo'] = pd.to_datetime(df_2['Periodo'], format='%Y-%m-%d', errors='coerce')
-
-# Uniformar el formato a dd/mm/yyyy (esto convierte a string)
-df_1['Periodo'] = df_1['Periodo'].dt.strftime('%d/%m/%Y')
-df_2['Periodo'] = df_2['Periodo'].dt.strftime('%d/%m/%Y')
+# Se convierten las columnas de tipo numérico a float
+df_link = flink.columnas_numericas(df_link, col_registros, col_ftd, col_valor_ftd, col_ftd_puntos_venta, col_ftd_pasarela)
 
 
-# print(df_1.tail(5))  # Mostrar las últimas 5 filas de df_1
-# print(df_2.head(5))  # Mostrar las primeras 5 filas de df_2
-
-
-# Unir los dos DataFrames de nuevo
-df = pd.concat([df_1, df_2], ignore_index=True)
-
-print(df.iloc[fecha_corte-2:].head(5))  # Mostrar 5 filas a partir de la fila "fecha_corte" del DataFrame final
-
-
-# # Encontrar el índice de la primera fila con dato nulo en la columna 'Periodo'
-# primer_null = df['Periodo'].isna() | (df['Periodo'] == '')
-# if primer_null.any():
-#     print(f"Número de la primera fila con dato Null en 'Periodo': {primer_null.idxmax()}")
-# else:
-#     print("No hay filas con datos Null en la columna 'Periodo'.")
-
-
-# # Contar cuántas filas vacías (NaN o cadenas vacías) tiene la columna "Periodo"
-# num_filas_vacias = df['Periodo'].isna().sum() + (df['Periodo'] == '').sum()
-# print(f"Número de filas vacías en la columna 'Periodo': {num_filas_vacias}")
-
-
-
-
-
-
-
+# Guardar el archivo CSV de links procesado en un archivo SQLite
+fsql.guardar_en_sqlite(df_link, tabla_links, ruta_db, if_exists="replace")
